c615bb11f38be71125ad21842001f65fe1447638
mac <mac@macs-Mac-mini.local>
2014-12-25 17:53:50 +0800

    Initial Commit

---
+++ IosDistributeSample/byAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  byAppDelegate.h
+//  IosDistributeSample
+//
+//  Created by mac on 12/25/14.
+//  Copyright (c) 2014 Beyondsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface byAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IosDistributeSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IosDistributeSample
+//
+//  Created by mac on 12/25/14.
+//  Copyright (c) 2014 Beyondsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "byAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([byAppDelegate class]));
+    }
+}
